42cef0e04d2fe3042242fb98d2b11caa56ce0dcd
Alakhdeep singh <54256747+Alakhdeepsingh@users.noreply.github.com>
2022-12-03 13:57:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let a = 10;
+console.log(a);
+a = "Harry";
+console.log(a);
+//Output
+// 10
+// Harry
+//this shows js is dynamically language because we can change it datatype like first it was number the it is string in it's runtime as we have changed a=10 to a="Harry"
+
+
+
+
